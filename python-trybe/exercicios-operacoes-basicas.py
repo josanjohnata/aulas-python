db25422e7c392a85dc0ec7fb179f00ad6302792b
josanjohnata <josan.pontes@gmail.com.br>
2022-01-08 21:47:48 -0300

    Adiciona exercicio 3

--- python-trybe/exercicios-operacoes-basicas.py
+++ python-trybe/exercicios-operacoes-basicas.py
@@ -10,8 +10,12 @@ result = a ** b # exponenciação
 result = a % b # módulo
 
 # Exercício 2: Declare e inicialize uma variável: hours = 6 . Quantos minutos têm em 6 horas? E quantos segundos? Declare e inicialize variáveis minutes e seconds que recebem os respectivos resultados das contas. Depois, imprima cada uma delas.
-hours = 6
-minutes = 60 * hours
-seconds = 60 * minutes
-print(minutes)
-print(seconds)
+hours = 6;
+minutes = 60 * hours;
+seconds = 60 * minutes;
+print(minutes);
+print(seconds);
+
+# Exercício 3: Teste e verifique o que acontece se você colocar um ponto e vírgula no final de uma instrução em Python.
+# No VS Code reclamou erro de lint, mas rodou normalmente e no terminal rodou sem reclamações.
+
