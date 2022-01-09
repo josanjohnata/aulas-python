9b840bb6700f9b1e55c044aef4171d681e4ce89f
josanjohnata <josan.pontes@gmail.com.br>
2022-01-08 23:00:28 -0300

    Adiciona exercicio 8

--- python-trybe/exercicios-operacoes-basicas.py
+++ python-trybe/exercicios-operacoes-basicas.py
@@ -38,3 +38,12 @@ trybe_course[0] = "Fundamentos"
 var = set()
 var.add('Josan Johnata')
 var
+
+# Exercício 8: O que acontecerá se você tentar acessar o valor da chave "personagem" como fazíamos em JavaScript, utilizando object.key ?
+info = {
+  "personagem": "Margarida",
+  "origem": "Pato Donald",
+  "nota": "Namorada do personagem principal nos quadrinhos do Pato Donald",
+}
+info.personagem
+# Resposta: Essa forma de acesso ao objeto em Python não é permitida, resultando em erro de sintaxe.
