b497f87c35674d49e320459d9ac3d856fecd63fc
josanjohnata <josan.pontes@gmail.com.br>
2022-01-08 23:20:45 -0300

    Adiciona exercicio 12

--- python-trybe/exercicios-operacoes-basicas.py
+++ python-trybe/exercicios-operacoes-basicas.py
@@ -58,3 +58,12 @@ info
 
 # Exercício 11: Após uma consulta do banco de dados, temos linhas que contém nome, sobrenome e idade como: "Thiago", "Nobre", 29 . Que estrutura vista anteriormente seria recomendada dado que após esta consulta somente exibimos estes valores.
 # Resposta: A estrutura recomendada é a tuple . Caso houvesse necessidade de editar os valores ou adicionar mais valores, usaríamos uma list.
+
+# Exercício 12: Realizar a contagem de quantas vezes cada elemento aparece em uma sequência é uma técnica muito útil na solução de alguns problemas. Qual é a estrutura mais recomendada para o armazenamento desta contagem?
+# Resposta: dict é a estrutura mais adequada, pois conseguimos armazenar o elemento da lista como chave e a quantidade de vezes que ele aparece como valor da chave.
+my_list = [20, 20, 1, 2];
+count_elements = {
+: 2,
+: 1,
+: 2
+}
