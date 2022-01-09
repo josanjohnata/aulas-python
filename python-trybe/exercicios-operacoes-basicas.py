a6f3278ea5661afb3210742dcbbc013019d26738
josanjohnata <josan.pontes@gmail.com.br>
2022-01-08 22:45:11 -0300

    Adiciona exercicio 6

--- python-trybe/exercicios-operacoes-basicas.py
+++ python-trybe/exercicios-operacoes-basicas.py
@@ -30,3 +30,6 @@ cost
 # Exercício 5: Adicione o elemento "Ciência da Computação" à lista.
 trybe_course = ["Introdução", "Front-end", "Back-end"]
 trybe_course.append("Ciência da Computação")
+
+# Exercício 6: Acesse e altere o primeiro elemento da lista para "Fundamentos".
+trybe_course[0] = "Fundamentos"
