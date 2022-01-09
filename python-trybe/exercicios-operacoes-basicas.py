d8974b3e10f4fdc123d55fe13461adff0023d9d7
josanjohnata <josan.pontes@gmail.com.br>
2022-01-08 21:40:39 -0300

    Adiciona exercicio 1

---
+++ python-trybe/exercicios-operacoes-basicas.py
@@ -0,0 +1,11 @@
+# Exercício 1: inicialize duas variáveis a e b, sendo a = 10 e b = 5. Mostre o resultado das 7 operações básicas (soma, subtração, multiplicação, divisão, divisão inteira, potenciação e módulo) envolvendo essas variáveis.
+a = 10
+b = 5
+result = a + b # adição
+result = a - b # subtração
+result = a * b # multiplicação
+result = a / b # divisão
+result = a // b # divisão inteira
+result = a ** b # exponenciação
+result = a % b # módulo
+
