e1f9717f9cfa466ea03a0db5cb6fdc77f168cf2c
josanjohnata <josan.pontes@gmail.com.br>
2022-01-08 22:15:23 -0300

    Adiciona exercicio 3

--- python-trybe/exercicios-operacoes-basicas.py
+++ python-trybe/exercicios-operacoes-basicas.py
@@ -18,4 +18,11 @@ print(seconds);
 
 # Exercício 3: Teste e verifique o que acontece se você colocar um ponto e vírgula no final de uma instrução em Python.
 # No VS Code reclamou erro de lint, mas rodou normalmente e no terminal rodou sem reclamações.
+# Assim como em JavaScript, Python permite que você coloque um ponto e vírgula no final de uma instrução.
 
+# Exercício 4: Suponha que o preço de capa de um livro seja 24,20, mas as livrarias recebem um desconto de 40%. O transporte custa 3,00 para o primeiro exemplar e 75 centavos para cada exemplar adicional. Qual é o custo total de atacado para 60 cópias? Escreva uma expressão que receba o custo total e a imprima.
+books = 60
+book_price = (1 - 0.4) * 24.20
+logistic = 3 + (books - 1) * 0.75
+cost = books * book_price + logistic
+cost 
