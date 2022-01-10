c9289bc4f86d76066f0b1efd10dd118a5cbaec28
josanjohnata <josan.pontes@gmail.com.br>
2022-01-09 23:07:05 -0300

    Add exercise bonus 1 bloco 32.1

---
+++ bonus.py
@@ -0,0 +1,18 @@
+# Exercício 1: Dada uma lista, descubra o menor elemento. Por exemplo, [5, 9, 3, 19, 70, 8, 100, 2, 35, 27] deve retornar 2 .
+
+
+def minimum(numb):
+    smaller = number[0]
+    for number in numb:
+        if number < smaller:
+            smaller = number
+    return smaller
+
+# Dica: a função min já existe nativamente no Python! Trabalhando em Python, sempre compensa pesquisar uma solução primeiro porque este brinquedo vem com todos os acessórios inclusos!
+
+
+def minimum2(numbers):
+   return min(numbers)
+
+# ou mesmo
+minimum2 = min
