22f3e8835833ec97e3237e226e4173181a82330d
josanjohnata <josan.pontes@gmail.com.br>
2022-01-09 23:45:24 -0300

    Add exercise bonus 3 bloco 32.1

--- bonus.py
+++ bonus.py
@@ -23,3 +23,12 @@ minimum2 = min
 def draw_triangle(n):
     for row in range(1, n + 1):
         print(row * '*')
+
+# Exercício 3: Crie uma função que receba um número inteiro N e retorne o somatório de todos os números de 1 até N . Por exemplo, para N = 5 , o valor esperado será 15.
+
+
+def summation(limit):
+    total = 0
+    for number in range(1, limit + 1):
+        total += number
+    return total
