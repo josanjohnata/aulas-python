4858bcbce22bc3212893866e65daa4608824554f
josanjohnata <josan.pontes@gmail.com.br>
2022-01-09 23:44:11 -0300

    Add exercise bonus 2 bloco 32.1

--- bonus.py
+++ bonus.py
@@ -16,3 +16,10 @@ def minimum2(numbers):
 
 # ou mesmo
 minimum2 = min
+
+# Exercício 2: Faça um programa que, dado um valor n qualquer, tal que n > 1 , imprima na tela um triângulo retângulo com n asteriscos de base. Por exemplo, para n = 5 o triângulo terá 5 asteriscos na base:
+
+
+def draw_triangle(n):
+    for row in range(1, n + 1):
+        print(row * '*')
