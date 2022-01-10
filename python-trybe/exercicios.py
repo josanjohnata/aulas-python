38a0680fca04772a0cecea4b3e6b8e9564db1c76
josanjohnata <josan.pontes@gmail.com.br>
2022-01-09 22:33:30 -0300

    Add exercise 1 bloco 32.1

---
+++ python-trybe/exercicios.py
@@ -0,0 +1,7 @@
+# Exercício 1: Crie uma função que receba dois números e retorne o maior deles.
+def biggest_number(a, b):
+    if a > b:
+        return a
+    return b
+
+    print(biggest_number(4, 7))
