df88994553ea10e5cefab0679e50291fc487b136
josanjohnata <josan.pontes@gmail.com.br>
2022-01-09 23:03:30 -0300

    Add exercise 6 bloco 32.1

--- python-trybe/exercicios.py
+++ python-trybe/exercicios.py
@@ -46,11 +46,30 @@ def paint_costs(area):
 
 # Uma alternativa mais direta, utilizando o módulo math
 
+
 import math
 
 
-def paint_costs(area):
+def paint_costs2(area):
     can_price = 80
     required_liters = area / 3
     required_cans = math.ceil(required_liters / 18)
     return required_cans, required_cans * can_price
+
+# Exercício 6: Crie uma função que receba os três lados de um triângulo e informe qual o tipo de triâgulo formado ou "não é triangulo" , caso não seja possível formar um triângulo.
+
+
+def type_of_triangle(side1, side2, side3):
+    is_triangle = (
+            side1 + side2 > side3 and
+            side2 + side3 > side1 and
+            side1 + side3 > side2
+    )
+    if not is_triangle:
+        return "não é triângulo"
+    elif side1 == side2 == side3:
+        return "equilátero"
+    elif side1 == side2 or side2 == side3 or side1 == side3:
+        return "isósceles"
+    else:
+        return "escaleno"
