a6563f98103995ffaa675e6c554c2abd3bb0e398
josanjohnata <josanjohnata@gmail.com>
2022-02-13 14:27:49 -0300

    Add exercicio 05

--- python-trybe/redes/tcp-udp/exercicios.py
+++ python-trybe/redes/tcp-udp/exercicios.py
@@ -100,3 +100,16 @@ if __name__ == "__main__":
     # usando with nosso TCPServer vai arrumar a casa direitinho quando encerrado
     with TCPServer(ADDRESS, EchoHandler) as server:
         server.serve_forever()
+
+# Exercício 5: Utilizando o comando telnet ou o Netcat (nc):
+
+# 1 - Conecte no server do exercício anterior e envie informações, o server
+# deverá imprimir as mensagens enviadas no console.
+# 2 - Pare o servidor e verifique o que aconteceu com a conexão que estava
+# aberta com o comando telnet ou nc.
+
+nc -t 127.0.0.1 8085
+
+# ou
+
+telnet 127.0.0.1 8085
