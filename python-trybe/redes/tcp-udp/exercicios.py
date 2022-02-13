d4b1c0c8fd00608ff46d7a36d535e8b06568138b
josanjohnata <josanjohnata@gmail.com>
2022-02-13 14:22:54 -0300

    Add exercicio 03

--- python-trybe/redes/tcp-udp/exercicios.py
+++ python-trybe/redes/tcp-udp/exercicios.py
@@ -45,3 +45,10 @@ curl google.com
 # porém agora utilizando o parâmetro -L que serve para "seguir redirecionamentos".
 
 curl -L google.com
+
+# Exercício 3: Agora utilizando o wget, pegue o conteúdo da página do site da
+# Trybe, depois abra o arquivo HTML baixado em seu navegador. Faça o mesmo
+# processo com outras páginas web.
+
+wget betrybe.com
+
