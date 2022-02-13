f8216fd7903425cc1502c55265567cbd20663d75
josanjohnata <josanjohnata@gmail.com>
2022-02-13 14:32:11 -0300

    Add exercicio 08

--- python-trybe/redes/tcp-udp/exercicios.py
+++ python-trybe/redes/tcp-udp/exercicios.py
@@ -161,3 +161,9 @@ class EchoHandler(DatagramRequestHandler):
 if __name__ == "__main__":
     with UDPServer(ADDRESS, EchoHandler) as server:
         server.serve_forever()
+
+# Exercício 8: Envie pacotes para o servidor UDP, utilizando o Netcat (nc).
+# Em seguida pare o servidor e perceba que como não há conexão nada é
+# sentido pelo client .
+
+nc -u 127.0.0.1 8084
