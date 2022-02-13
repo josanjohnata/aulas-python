9e05c5dbf46057ec32f6c0fc3eb6d4998e9a2db5
josanjohnata <josanjohnata@gmail.com>
2022-02-13 14:29:23 -0300

    Add exercicio 06

--- python-trybe/redes/tcp-udp/exercicios.py
+++ python-trybe/redes/tcp-udp/exercicios.py
@@ -113,3 +113,19 @@ nc -t 127.0.0.1 8085
 # ou
 
 telnet 127.0.0.1 8085
+
+# Exercício 6: Reinicie o servidor TCP e agora faça uma requisição utilizando
+# o cURL (HTTP), perceba o que é exibido no console do server já que não
+# estamos utilizando o HTTP nele, perceba também que o comando cURL não
+# recebe uma resposta HTTP com sentido (um status code 200, por exemplo),
+# de modo que ele não sabe que aquela requisição chegou ao fim.
+
+curl localhost:8085
+
+# uma request mais legal:
+
+curl --request POST \
+    --data "{ \"foo\": \"bar\" }" \
+    --header 'Content-Type: application/json' \
+    --header 'Foo-Bar-Header: foo-bar' \
+    localhost:8085/foo-bar
