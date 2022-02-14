5aa50b70424f91d417b4dbf54db8225154e73f2e
josanjohnata <josanjohnata@gmail.com>
2022-02-14 08:47:42 -0300

    Add exercicio 01

---
+++ python-trybe/redes/SI-redes/exercicios.py
@@ -0,0 +1,10 @@
+# Exercício 1: Defina uma regra de firewall utilizando o comando
+# iptables -A que bloqueie (block ou REJECT/DROP) toda a entrada
+# (in ou INPUT) de pacotes utilizando o protocolo ICMP, impedindo
+# assim que a máquina responda ao comando ping. Lembre-se, você
+# pode executar o comando ping para validar se sua regra está
+# funcionando corretamente: ping 127.0.0.1 (você pode adicionar
+# o parâmetro -O para exibir os pings rejeitados também).
+
+iptables -A INPUT -p icmp -j REJECT
+
