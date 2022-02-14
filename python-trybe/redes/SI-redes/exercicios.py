f771feeb017167b8cb3be94aa4ea821760360332
josanjohnata <josanjohnata@gmail.com>
2022-02-14 08:52:18 -0300

    Add exercicio 04

--- python-trybe/redes/SI-redes/exercicios.py
+++ python-trybe/redes/SI-redes/exercicios.py
@@ -21,3 +21,10 @@ iptables -D INPUT -p icmp -j REJECT
 # ou o Facebook (via curl, se estiver rodando no docker).
 
 iptables -A OUTPUT -p tcp --sport 443 -j REJECT
+
+# Exercício 4: Bloqueie agora o tráfego de saída para HTTP.
+# Lembre-se, também é utilizado o protocolo tcp e a porta 80.
+# Para testar sua regra, tente acessar um site pelo navegador
+# que use HTTP .
+
+iptables -A OUTPUT -p tcp --sport 80 -j REJECT
