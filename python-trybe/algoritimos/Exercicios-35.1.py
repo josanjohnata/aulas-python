ea68e9b0466cf13e4f38dbcdb2666c9e2af1e11d
josanjohnata <josanjohnata@gmail.com>
2022-02-20 11:22:37 -0300

    Exercicios de fixaçao - Algoritimos 35.1 - 02

--- python-trybe/algoritimos/Exercicios-35.1.py
+++ python-trybe/algoritimos/Exercicios-35.1.py
@@ -21,3 +21,21 @@ def contains_duplicate(numbers):
         previous_number = number
 
     return False
+
+
+# Exercício 2: Suponha que se está escrevendo uma função para um jogo de
+# batalha naval. Dado um array bidimensional com n linhas e m colunas, e um
+# par de coordenadas x para linhas e y para colunas, o algoritmo verifica se
+# há um navio na coordenada alvo. Por exemplo:
+
+
+""" Mesmo para um array bidimensional, o acesso a um elemento é O(1).
+A complexidade de espaço também é O(1), pois não consideramos a entrada em seu
+cálculo."""
+
+
+def battleship(grid, line, column):
+    if(grid[line][column] == 1):
+        return True
+
+    return False
